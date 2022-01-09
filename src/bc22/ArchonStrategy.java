661175eae441c11aa8c22e33048f34aeef9e466d
hachibits <olxu@hotmail.com>
2022-01-09 22:50:54 +1100

    change miners soldiers ratio

--- src/bc22/ArchonStrategy.java
+++ src/bc22/ArchonStrategy.java
@@ -12,9 +12,9 @@ public strictfp class ArchonStrategy {
             buildTowardsLowRubble(rc, RobotType.SOLDIER);
         } else if (builders < 1) {
             buildTowardsLowRubble(rc, RobotType.BUILDER);
-        } else if (miners < soldiers / 2 && rc.getTeamLeadAmount(rc.getTeam()) < 5000) {
+        } else if (miners < soldiers * (6/11) && rc.getTeamLeadAmount(rc.getTeam()) < 5000) {
             buildTowardsLowRubble(rc, RobotType.MINER);
-        } else if (builders < soldiers / 10) {
+        } else if (builders < soldiers / 30) {
             buildTowardsLowRubble(rc, RobotType.BUILDER);
         } else {
             buildTowardsLowRubble(rc, RobotType.SOLDIER);
